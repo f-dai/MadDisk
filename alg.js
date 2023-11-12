7c3a2457d3291102b90e22e40832c8d9ac3aa63d
jamesjaw <50652557+jamesjaw@users.noreply.github.com>
2023-11-12 00:01:31 -0600

    Update alg.js

--- alg.js
+++ alg.js
@@ -30,3 +30,55 @@ class SSTF {
     return closestIndex;
   }
 }
+
+
+class SCAN {
+  constructor() {
+    this.requests = new Array(32).fill(0); 
+    this.direction = 1; 
+    this.pointer = 0;
+  }
+
+  init() {
+    this.requests.fill(0);
+    this.direction = 1; 
+    this.pointer = 0; 
+  }
+
+  isEmpty() {
+    return !this.requests.some(r => r === 1);
+  }
+
+  add(request) {
+    this.requests[request] = 1;
+  }
+
+  get() {
+    if(this.isEmpty()) {
+      return -1;
+    }
+
+    while(true) {
+      if(this.direction === 1) {
+        for(; this.pointer < 32; this.pointer++) {
+          if(this.requests[this.pointer] === 1) {
+            this.requests[this.pointer] = 0;
+            return this.pointer;
+          }
+        }
+        this.pointer--;
+        this.direction = 0;
+      }
+      else {
+        for(; this.pointer >= 0; this.pointer--) {
+          if(this.requests[this.pointer] === 1) {
+            this.requests[this.pointer] = 0;
+            return this.pointer;  
+          }
+        }
+        this.pointer++;
+        this.direction = 1;
+      }
+    }
+  }
+}
